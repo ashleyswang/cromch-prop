46bd3b337007ccd2f59be4c761d7d6d1cea99410
Ashley Wang <ashleyswang@postoffice.intern>
2022-11-17 17:02:26 -0800

    remove bin

--- cpp/bin/obj/ift.d
+++
@@ -1,3 +0,0 @@
-bin/obj/ift.o: src/ift.cpp include/ift.h
-
-include/ift.h:
--- cpp/bin/obj/multithread_propagate.d
+++
@@ -1,6 +0,0 @@
-bin/obj/multithread_propagate.o: src/multithread_propagate.cpp \
-  include/propagate_flags.h include/ift.h
-
-include/propagate_flags.h:
-
-include/ift.h:
